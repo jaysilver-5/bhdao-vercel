03011b7c7a4c16516b97036813a59cffa25b6cbf
Joshua John U. <73034334+jaysilver-5@users.noreply.github.com>
2026-02-28 21:55:48 +0100

    Update CORS origin to localhost for development

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'https://bhdao-vercel-qrr8wv8kp-joshs-projects-e9dac4f7.vercel.app',
+    origin: 'https://localhost:3001',
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
